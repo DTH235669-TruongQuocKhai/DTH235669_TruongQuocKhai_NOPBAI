7e3d935fc10819ec3a9b4d840ff80e762f0f5e1c
DTH235669-TruongQuocKhai <email_github_cua_ban@gmail.com>
2025-09-04 22:46:52 +0700

    Update new homework

---
+++ BaiTap_Python/Chuong_2/Cac_BaiTap/Bai_1.py
@@ -0,0 +1,10 @@
+#Tính chu vi và diện tích hình tròn
+import math
+try:
+    r= float (input("Nhập bán kính hình tròn:"))
+    cv=2*math.pi*r
+    dt=math.pi*r*r
+    print("Chu vi=",cv)
+    print("Diện tích=",dt)
+except:
+    print("Lỗi rồi!")
